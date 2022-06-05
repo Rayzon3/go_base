714486fca6377350470101410441543fdeec1e88
Rahul Bhardwaj <rahul.03111999@gmail.com>
2022-06-05 18:32:12 +0530

    Basic init and setup 🛠

--- main.go
+++ main.go
@@ -1,1 +1,50 @@
 package main
+
+type coords struct {
+	row uint
+	col uint
+}
+
+//Keywords for query
+type keyword string
+
+const (
+	selectKeyword keyword = "select"
+	fromKeyword   keyword = "from"
+	asKeyword     keyword = "as"
+	tableKeyword  keyword = "table"
+	createKeyword keyword = "create"
+	insertkeyword keyword = "insert"
+	intokeyword   keyword = "into"
+	valuesKeyword keyword = "values"
+	intKeyword    keyword = "int"
+	textKeyword   keyword = "text"
+	boolKeyword   keyword = "bool"
+)
+
+//Symbols for query
+type symbol string
+
+const (
+	semicolonSymbol  symbol = ";"
+	starSymbol       symbol = "*"
+	commaSymbol      symbol = ","
+	leftParenSymbol  symbol = "("
+	rightParenSymbol symbol = ")"
+)
+
+type tokenKind uint
+
+const (
+	keywordKind tokenKind = iota
+	symbolKind
+	identifierKind
+	stringkind
+	numericKind
+)
+
+type token struct {
+	value string
+	kind  tokenKind
+	loc   coords
+}
